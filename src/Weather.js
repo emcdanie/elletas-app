a054adfe2eaa46bdc25dadb13bc261c6932ffc08
emcdanie <66241957+emcdanie@users.noreply.github.com>
2020-09-14 14:32:32 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -28,65 +28,63 @@ export default function Weather(props) {
   function handleCityChange(event) {
     setCity(event.target.value);
   }
-  function search(){
-  const apiKey = "717c122d03da5b502d476732c8793a31";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  function search() {
+    const apiKey = "717c122d03da5b502d476732c8793a31";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(showWeather);
-}
+    axios.get(apiUrl).then(showWeather);
+  }
   if (weatherData.ready) {
-return (
-    <div className="Weather">
-      <div class="row">
-        <div class="col-12">
-          <h1> {weatherData.city} </h1>
-          <ul>
-            <li class="title">Today </li>
-            <li> {weatherData.date} </li>
-            <li>
-              <span>{weatherData.description}</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <br />
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src={weatherData.imgUrl}
-              alt={"sunhappy.svg"}
-              className="weather-icon"
-            />
-            <div className="float-right">
-              <span className="temperature">{weatherData.temperature}</span>
-              <span className="units">
-                <a href="/" className="link">
-                  °C
-                </a>{" "}
-                |
-                <a href="/" className="link">
-                  °F
-                </a>
-              </span>
-            </div>
+    return (
+      <div className="Weather">
+        <div class="row">
+          <div class="col-12">
+            <h1> {weatherData.city} </h1>
+            <ul>
+              <li class="title">Today </li>
+              <li> {weatherData.date} </li>
+              <li>
+                <span>{weatherData.description}</span>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="col-6">
-          <ul>
-            <li className="current-data">
-              {" "}
-              Feels Like: {weatherData.feelslike} °C
-            </li>
-            <li className="current-data">Humidity: {weatherData.humidity} %</li>
-            <li className="current-data">Wind: {weatherData.wind} km/h</li>
-          </ul>
+        <br />
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix">
+              <img src={weatherData.imgUrl} alt={} className="weather-icon" />
+              <div className="float-right">
+                <span className="temperature">{weatherData.temperature}</span>
+                <span className="units">
+                  <a href="/" className="link">
+                    °C
+                  </a>{" "}
+                  |
+                  <a href="/" className="link">
+                    °F
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li className="current-data">
+                {" "}
+                Feels Like: {weatherData.feelslike} °C
+              </li>
+              <li className="current-data">
+                Humidity: {weatherData.humidity} %
+              </li>
+              <li className="current-data">Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-    
+    );
   } else {
-  search();
-  return "Loading...";
-}
+    search();
+    return "Loading...";
+  }
 }
